18cb9a22671da470848a51a2e430c9fd5f2e22a0
leiqlxk <472990991@qq.com>
2021-03-03 21:50:09 +0800

    目录结构划分

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    //去除校验函数括号前的空格
+    'space-before-function-paren':0
   }
 }
